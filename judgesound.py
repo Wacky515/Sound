f9a444a447494764c77fe352304f4f44f5448d36
wacky515@gmail.com <firebomberd7>
2016-07-01 07:24:08 +0900

    v

---
+++ judgesound.py
@@ -0,0 +1,53 @@
+﻿# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------------
+# Name:        JudgeSound
+# Purpose:
+#
+# Author:      Kilo11
+#
+# Created:     24/03/2016
+# Copyright:   (c) SkyDog 2016
+# Licence:     SDS10008
+# -------------------------------------------------------------------------------
+
+# モジュールインポート
+import winsound
+import time
+
+import sys
+# sysモジュール リロード
+reload(sys)
+
+# デフォルトの文字コード 出力
+sys.setdefaultencoding("utf-8")
+
+
+class JudgeSound:
+    """ OK/NG判定音 出力 """
+    def beep_ok(self):
+        """ OK判定音 出力 """
+        winsound.Beep(2000, 1000)
+        print("OK!")
+        print("")
+
+    def beep_ng(self):
+        """ NG判定音 出力 """
+        for times in range(2):
+            winsound.Beep(2000, 500)
+            time.sleep(0.1)
+        print("NG!")
+        print("")
+
+
+def main():
+    js = JudgeSound()
+
+    js.beep_ok()
+    time.sleep(0.5)
+
+    js.beep_ng()
+    time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
---
+++ modtjudgesound.py
@@ -0,0 +1,33 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        ModuleTestJudgeSound
+# Purpose:
+#
+# Author:      Kilo11
+#
+# Created:     01/04/2016
+# Copyright:   (c) SkyDog 2016
+# Licence:     SDS10008-1
+#-------------------------------------------------------------------------------
+#!/usr/bin/python
+# デフォルトの文字コード 変更
+# -*- coding: utf-8 -*-
+
+# モジュールインポート
+import judgesound as js
+
+import sys
+# sysモジュール リロード
+reload(sys)
+
+# デフォルトの文字コード 出力
+sys.setdefaultencoding("utf-8")
+
+js = js.JudgeSound()
+js.OkBeep()
+js.NgBeep()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
